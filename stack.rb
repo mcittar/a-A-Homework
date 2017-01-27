a7dba9877e21ddff553b4b1bfe06c90b919397ef
Michael Cittar <mcittar@Michaels-MacBook-Air.local>
2017-01-26 20:32:54 -0800

    Implement stack class

---
+++ stack.rb
@@ -0,0 +1,22 @@
+class Stack
+  attr_reader :ivar
+  def initialize(ivar)
+    # create ivar to store stack here
+    @ivar = [ivar]
+  end
+
+  def add(el)
+    # adds an element to the stack
+    ivar.push(el)
+  end
+
+  def remove
+    # removes one element from the stack
+    ivar.pop
+  end
+
+  def show
+    # return a copy of the stack
+    ivar
+  end
+end
